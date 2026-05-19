8f8e9f7774847b1ab3ee86988b729ac14109e8f6
Jonas Abeln <jonas.abelnx@gmail.com>
2026-05-19 20:14:00 +0200

    Auto update(swUpdate) for downloaded app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, signal, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 import { EVCalculator } from "./ev-calculator/ev-calculator";
 import { BlackJackGame } from './black-jack-game/black-jack-game';
 
@@ -10,6 +11,22 @@ import { BlackJackGame } from './black-jack-game/black-jack-game';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   Active_screen = "main"
+
+  // SWUpdate
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    // active checking for updates
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+      .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(() => {
+        if (confirm('New version  of EDGE 21: BlackJack is available! Refresh app?')) {
+          window.location.reload();
+        };
+      });
+    }
+  }
 }
