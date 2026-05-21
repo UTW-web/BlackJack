2cfe6b673dded373e1b6d300ce29de385000d98a
Jonas Abeln <jonas.abelnx@gmail.com>
2026-05-21 19:49:29 +0200

    Added Settings page and buttons for blackjack

--- src/app/black-jack-game/black-jack-game.ts
+++ src/app/black-jack-game/black-jack-game.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface DataItem {
+  y: number;
+  x: number;
+}
 
 @Component({
   selector: 'app-black-jack-game',
@@ -6,6 +11,39 @@ import { Component } from '@angular/core';
   templateUrl: './black-jack-game.html',
   styleUrl: './black-jack-game.css',
 })
-export class BlackJackGame {
+export class BlackJackGame implements OnInit {
+  
+  active_screen = "rules";
+  
+  StartGame(type: string) {
+    this.active_screen = type;
+  }
+  ngOnInit(): void {
+    this.engine();
+  };
 
-}
+  
+  engine() {
+    //rules
+
+    // Betting
+    let BET
+    //Number of decks
+    let NOD
+    //Dealer hits soft 17
+    let DHS17
+    //Can Double After Split
+    let CDAS
+    //Doubling Available
+    let DA
+    //resplitting aces
+    let RA
+    //Surrender
+    let SURR
+    //cards
+
+    //input
+
+    //
+  };
+};
